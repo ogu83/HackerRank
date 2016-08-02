6b254792e9dd894e62e25ba2f4f3d43576340656
Oğuz Köroğlu <korogu@hotmail.com>
2016-08-02 23:01:04 +0300

    Project Euler #13: Large sum

---
+++ HackerRank/euler1/Problem13.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace euler1
+{
+    /// <summary>
+    /// Project Euler #13: Large sum    
+    /// </summary>
+    public class Problem13
+    {
+        public static void Exec()
+        {
+            var row = int.Parse(Console.ReadLine());
+            BigInteger sum = 0;
+            for (int r = 0; r < row; r++)
+            {
+                var NN = Console.ReadLine();
+                BigInteger N;
+                BigInteger.TryParse(NN, out N);
+                sum += N;
+            }
+            Console.WriteLine(sum.ToString().Length > 10 ? sum.ToString().Substring(0, 10) : sum.ToString());
+        }
+    }
+}
